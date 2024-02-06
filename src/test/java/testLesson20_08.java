7d197cf327147fa9a8b24e1e7735c0ae295a6cf0
Stas <bestman-2018@mail.ru>
2024-02-06 15:48:27 +0300

    работа с заголовками

---
+++ src/test/java/testLesson20_08.java
@@ -0,0 +1,31 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class testLesson20_08 {
+    @Test
+    public void testHelloWorldLesson20_08(){
+        Map<String,String> headers = new HashMap<>();
+        headers.put("myHeader1", "myValue1");
+        headers.put("myHeader2", "myValue2");
+
+        Response response = RestAssured
+
+                .given()
+                .headers(headers)
+                .when()
+                .get("https://playground.learnqa.ru/api/show_all_headers")
+                .andReturn();
+
+        response.prettyPrint(); /* заголовок что МЫ шлём серверу */
+
+        Headers responseHeaders = response.getHeaders();
+        System.out.println(responseHeaders); /* заголовок что НАМ прислал сервер */
+
+    }
+}
