b951d9277131d5bc11523d874004fb8c89759959
Stas <bestman-2018@mail.ru>
2024-02-28 18:02:38 +0300

    registered new user

--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -3,6 +3,7 @@ package tests;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import lib.Assertions;
+import lib.DataGeneration;
 import lib.baseTestcase;
 import org.junit.jupiter.api.Test;
 
@@ -30,5 +31,26 @@ public class UserRegisterTest extends baseTestcase {
 
         Assertions.assertResponseStatuscodeEquals(responseCreateAuth,400);
         Assertions.assertResponseTextEquals(responseCreateAuth,"Users with email '" + email + "' already exists");
+    }
+    @Test
+    public void testCreateUserSuccessfully() {
+        String email = DataGeneration.getRandomEmail();
+
+
+        Map<String,String> userData = new HashMap<>();
+        userData.put("username","learnqa");
+        userData.put("firstName","learnqa");
+        userData.put("lastName","learnqa");
+        userData.put("email",email);
+        userData.put("password","123");
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseStatuscodeEquals(responseCreateAuth,200);
+        Assertions.assertJsonHasKey(responseCreateAuth,"id");
     }
 }
