629cec8d861efddd5533e86cab06c3016a9d0af4
Stas <bestman-2018@mail.ru>
2024-02-28 14:54:43 +0300

    create new User with registered email

---
+++ src/test/java/tests/UserRegisterTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.baseTestcase;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserRegisterTest extends baseTestcase {
+    @Test
+    public void testCreateUserWithExistingEmail() {
+        String email = "vinkotov@example.com";
+
+
+        Map<String,String> userData = new HashMap<>();
+        userData.put("username","learnqa");
+        userData.put("firstName","learnqa");
+        userData.put("lastName","learnqa");
+        userData.put("email",email);
+        userData.put("password","123");
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseStatuscodeEquals(responseCreateAuth,400);
+        Assertions.assertResponseTextEquals(responseCreateAuth,"Users with email '" + email + "' already exists");
+    }
+}
