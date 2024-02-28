babee8e03d479e85e15613bd35e8af66a7a9584e
Stas <bestman-2018@mail.ru>
2024-02-28 18:01:44 +0300

    add create email

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -5,10 +5,28 @@ import static org.hamcrest.Matchers.hasKey;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
-    public static void assertJsonByName(Response response,String name,int expectedValue){
-        response.then().assertThat().body("$",hasKey(name));
+    public static void assertJsonByName(Response response, String name, int expectedValue) {
+        response.then().assertThat().body("$", hasKey(name));
         int value = response.jsonPath().getInt(name);
-        assertEquals(expectedValue,value,"JSON value is not equals to expected value");
+        assertEquals(expectedValue, value, "JSON value is not equals to expected value");
+    }
 
+    public static void assertResponseTextEquals(Response response, String expectedAnswer) {
+        assertEquals(
+                expectedAnswer,
+                response.asString(),
+                "Response text is not as expected"
+        );
+    }
+
+    public static void assertResponseStatuscodeEquals(Response response, int expectedStatuscode) {
+        assertEquals(
+                expectedStatuscode,
+                response.statusCode(),
+                "Response Statuscode is not as expected"
+        );
+          }
+    public static void assertJsonHasKey(Response response, String expectedFieldName) {
+        response.then().assertThat().body("$",hasKey(expectedFieldName));
     }
 }
