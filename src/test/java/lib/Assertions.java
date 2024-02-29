ed8fb0a903f95d675643030138a8be50145771be
Stas <bestman-2018@mail.ru>
2024-02-29 19:16:33 +0300

    rename and add massive

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -2,6 +2,7 @@ package lib;
 
 import io.restassured.response.Response;
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
@@ -26,7 +27,17 @@ public class Assertions {
                 "Response Statuscode is not as expected"
         );
           }
-    public static void assertJsonHasKey(Response response, String expectedFieldName) {
+    public static void assertJsonHasField(Response response, String expectedFieldName) {
         response.then().assertThat().body("$",hasKey(expectedFieldName));
+    }
+
+    public static void assertJsonHasFields(Response response, String[] expectedFieldNames){
+        for(String expectedFieldName : expectedFieldNames){
+            Assertions.assertJsonHasNotField(response, expectedFieldName);
+        }
+    }
+
+    public static void assertJsonHasNotField(Response response,String unexpectedFieldName){
+        response.then().assertThat().body("$",not(hasKey(unexpectedFieldName)));
     }
 }
